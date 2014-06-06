9dc67abb8fb5af093979eaa2678dd4bb04f49d28
suisho <suisho000000@gmail.com>
2014-06-07 00:47:00 +0900

    inital

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'example-kitc'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures example-kitc'
+long_description 'Installs/Configures example-kitc'
+version          '0.1.0'
+
+depends "example-recipe"
